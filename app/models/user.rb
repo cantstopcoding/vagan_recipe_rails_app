2683b7d24ed99434ce4d9abecfde47f1036f491f
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-02 14:33:40 -0400

    make category only have many resipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,4 @@ class User < ApplicationRecord
   has_many :comments
   has_many :recipes
   has_many :commented_recipes, through: :comments, source: :recipes
-  has_many :categories, through: :recipes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,8 @@ ActiveRecord::Schema.define(version: 2021_05_02_181314) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -54,7 +52,6 @@ ActiveRecord::Schema.define(version: 2021_05_02_181314) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "categories", "users"
   add_foreign_key "comments", "recipes"
   add_foreign_key "comments", "users"
   add_foreign_key "recipes", "categories"
