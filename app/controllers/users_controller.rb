200a31e0454219286fb263ce032a0a4b452d5949
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-03 17:54:12 -0400

    redirect if user does not exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
-      redirect_to "/users/#{@user.id}"
+      redirect_to user_path(@user.id)
     else
       rende :new
     end
   end
 
   def show
+    # what is the difference between find and find_by?
+    # find_by_id returns nil and find_by returns error
+    @user = User.find_by_id(params[:id])
+    redirect_to "/" if !@user
   end
 
   private
