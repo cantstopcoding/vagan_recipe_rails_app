a1db997c8dd6aa2b756bc52d3c98b8fcd30cda30
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-04 13:48:47 -0400

    make a create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,18 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  def create
+    user = User.find_by(email: params[:user][:email])
+
+    # you need to find a user in order to authenticate
+    if user && user.authenticate(params[:user][:password])
+      # binding.pry
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Wrong Info :( Please Try Again"
+      redirect_to login_path
+    end
+  end
 end
