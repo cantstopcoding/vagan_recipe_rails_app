8460ad731121879798d1a9066d1103ff8e9c3bf0
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-04 18:27:54 -0400

    make not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   # gives access to these methods to the views
+  # can also put in application helper but it's going to be mostly use in the controllers so it's appropriate to use in controllers
   helper_method :current_user, :logged_in?
 
   private # but why private?
