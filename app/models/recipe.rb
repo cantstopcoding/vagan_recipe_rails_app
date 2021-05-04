4e09a7735f10697116369bd8d35d3845482a268f
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-04 18:30:44 -0400

    get rid of category association to recipw for now

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  # belongs_to :category
   has_many :comments
   has_many :commented_users, through: :comments, source: :user
 end
