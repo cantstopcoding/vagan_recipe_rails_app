b16bf6f469c9f947ad4940e25ef3293505da4a62
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-04 18:10:25 -0400

    add helper methods so view page can access them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  # gives access to these methods to the views
+  helper_method :current_user, :logged_in?
+
+  private # but why private?
+
   def current_user
     @current_user ||= User.find_by_id(:user_id) if session[:user_id]
   end
