c02f44fba684bf27e4648d6a0aa8e1ea85cd9ee2
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-05 16:11:34 -0400

    fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private # but why private?
 
   def current_user
-    @current_user ||= User.find_by_id(:user_id) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
