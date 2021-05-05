a3012bf43a1a7559c4abb0c9d3c42ef04cd418c9
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-05 16:54:42 -0400

    define show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,18 @@
 class RecipesController < ApplicationController
   before_action :redirect_if_not_logged_in
 
+  # def index
+  #   if params[:user_id] && @user = User.find_by_id(params[:user_id])
+  #     @posts = @user.posts.alpha
+  #   else
+  #     @error = "That user doesn't exist" if params[:user_id]
+  #     @posts = Post.alpha.includes(:category, :user)
+  #   end
+
+  #   @posts = @posts.search(params[:q].downcase) if params[:q] && !params[:q].empty?
+  #   @posts = @posts.filter(params[:post][:category_id]) if params[:post] && params[:post][:category_id] != ""
+  # end
+
   def new
     @recipe = Recipe.new
   end
@@ -12,6 +24,11 @@ class RecipesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @recipe = Recipe.find_by_id(params[:id])
+    redirect_to recipes_path if !@recipe
   end
 
   private
