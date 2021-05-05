153abab9d5e3ccfbe6aa67b8221f36de0bbf7882
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-05 18:23:29 -0400

    create actions for comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    binding.pry
+    if @comment.save
+      redirect_to comments_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.find_by_id(params[:id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:recipe_id, :content)
+  end
 end
