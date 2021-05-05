e0b468c30a057df3177ba36e04cd7dcd29f66d85
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-05 16:26:12 -0400

    create actions in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,22 @@
 class RecipesController < ApplicationController
-  redirect_if_not_logged_in
+  before_action :redirect_if_not_logged_in
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :image_url, :ingredients, :instructions)
+  end
 end
