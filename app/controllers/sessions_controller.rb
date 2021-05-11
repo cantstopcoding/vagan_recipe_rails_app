8e3256112e65117f6a4e7063e632d4d7285e0e96
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-11 17:12:12 -0400

    create google omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,26 @@ class SessionsController < ApplicationController
       redirect_to login_path
     end
   end
+
+  def google
+    @user = User.find_or_create_by(email: auth[:info][:email]) do |user|
+      user.first_name = auth[:info][:first_name]
+      user.last_name = auth[:info][:last_name]
+      user.username = auth[:info][:first_name]
+      user.password = SecureRandom.hex
+    end
+    # why am I seting a password?
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to "/"
+    end
+  end
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   delete "logout" => "sessions#destroy"
 
+  get "/auth/:provider/callback" => "sessions#google"
+
   resources :categories
   resources :comments
   resources :users do
