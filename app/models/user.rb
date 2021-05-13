f5ef11eca989a2bec61a4b6f3cac3547934355c2
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-13 18:45:35 -0400

    create validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :recipes
   has_many :commented_recipes, through: :comments, source: :recipe
-
   has_secure_password
+
+  validates :first_name, :last_name, :username, :email, presence: true
 end
