da9b5a683e4a823afb761bc459d6c38d10046454
cantstopcoding <jonathan_koss@yahoo.com>
2021-05-14 10:04:51 -0400

    create error partials

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   belongs_to :category
   has_many :comments
   has_many :commented_users, through: :comments, source: :user
-  validates :title, :image_url, :ingredients, :instructions, presence: true
+  validates :title, :image_url, :ingredients, :instructions, :category_id, presence: true
 
   scope :alpha, -> { order(:title) }
   scope :most_commented, -> { left_joins(:comments).group("recipes.id").order("count(recipes.id) desc") }
